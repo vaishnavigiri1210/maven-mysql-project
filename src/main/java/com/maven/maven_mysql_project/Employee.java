f7379db84001046d2d2cb01e8e46bfc755e31606
Vaishnavi Giri <vaishnavigiri1210@gmail.com>
2025-06-25 20:34:06 +0530

    updated code

--- src/main/java/com/maven/maven_mysql_project/Employee.java
+++ src/main/java/com/maven/maven_mysql_project/Employee.java
@@ -5,9 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 // import lombok.Getter;
 // import lombok.Setter;
+// import lombok.ToString;
+// import lombok.EqualsAndHashCode;
 
 // @Setter
 // @Getter
+// @ToString
+// @EqualsAndHashCode
 @AllArgsConstructor //parameterized constructor
 @Data   //it includes @ToString, @EqualsAndHashCode, @Getter, @Setter
 @NoArgsConstructor // default constructor
--- src/main/java/com/maven/maven_mysql_project/JDK8features.java
+++ src/main/java/com/maven/maven_mysql_project/JDK8features.java
@@ -1,5 +1,6 @@
 package com.maven.maven_mysql_project;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,14 +18,15 @@ public class JDK8features {
     public static void main(String[] args) {
 
         //1,2,3,4,5,6,7>1.8
-
-        /* String name[] = {"Ravi", "Raju", "Ramesh", "Suresh", "Mahesh"};
+        String name[] = {"Ravi", "Raju", "Ramesh", "Suresh", "Mahesh"};
 
         for (int i = 0; i < name.length; i++) {
             if (!name[i].equalsIgnoreCase("Raju")) {
                 System.out.println(name[i]);
             }
         }
+        System.out.println("-----------------------------------");
+
         // Collection
         List<String> names = new ArrayList<>();
         names.add("Ravi");
@@ -34,32 +36,42 @@ public class JDK8features {
         names.add("Mahesh");
         // names.add(50); // Removed to ensure all elements are Strings
         System.out.println(names);
+        System.out.println("-----------------------------------");
 
-        System.out.println("New approach.........................");
-        names.stream().forEach(System.out::println); */
+        System.out.println("New approach...");
+        names.stream().forEach(System.out::println);
+        System.out.println("-----------------------------------");
+
+        // Fetching employee data from Database
         List<Employee> employeeList = DatabaseConnection.getEmployee();
-        // System.out.println(employeeList);
+        System.out.println(employeeList);
+        System.out.println("-----------------------------------");
 
-        //legacy approach
-        /* for (Employee e : employeeList) {
+        // legacy approach
+        for (Employee e : employeeList) {
             if (e.getDepartment().equals("IT")) {
                 System.out.println(e);
             }
-        }  */
+        }
+        System.out.println("-----------------------------------");
+
         // JDK 8 approach using Streams API
-        // System.out.println("--------------------");
         // filtering the employee list based on variables(various conditions)
-        // employeeList.stream().filter(s -> s.getSalary() > 50000 && s.getEmpName().startsWith("R")).forEach(System.out::println);
-        // employeeList.stream().map(t -> t.getDepartment().toUpperCase()).forEach(System.out::println);
-        // System.out.println("-----------------------------------");
+        employeeList.stream().filter(s -> s.getSalary() > 50000 && s.getEmpName().startsWith("R")).forEach(System.out::println);
+        System.out.println("-----------------------------------");
+
+        employeeList.stream().map(t -> t.getDepartment().toUpperCase()).forEach(System.out::println);
+        System.out.println("-----------------------------------");
+
         List<String> s1 = Arrays.asList("A", "B", "C", "A", "D", "B");
-        // s1.stream().distinct().forEach(System.out::println); // to remove duplicates
-        // System.out.println("-----------------------------------");
+        s1.stream().distinct().forEach(System.out::println); // to remove duplicates
+        System.out.println("-----------------------------------");
 
         List<Integer> num = Arrays.asList(2, 4, 1, 7, 3, 5, 6);
         Collections.sort(num); // sorting the list i.e. ascending order
         Collections.reverse(num); // reversing the list i.e. descending order
-        // System.out.println(num);
+        System.out.println(num);
+        System.out.println("-----------------------------------");
 
         // JDK 1.8 approach using Lambda Expression
         // sorting employee list based on empName i.e. string using Lambda Expression
@@ -75,16 +87,19 @@ public class JDK8features {
         // sorting employee list based on salary using MyComparator class
         Collections.sort(employeeList, new MyComparator());
         employeeList.stream().forEach(System.out::println);
+        System.out.println("-----------------------------------");
 
-        /* Employee e1 = new Employee();
+        Employee e1 = new Employee();
         e1.setEmpID(5);
         e1.setEmpName("John");
         e1.setDepartment("IT");
         e1.setSalary(60000);
-        /* System.out.println(e1.getEmpID());
+        System.out.println(e1.getEmpID());
         System.out.println(e1.getEmpName());
         System.out.println(e1.getDepartment());
         System.out.println(e1.getSalary());
-        System.out.println(e1);   // Using toString() method from @Data annotation        */
+        System.out.println("-----------------------------------");
+        System.out.println(e1);   // Using toString() method from @Data annotation        
+        System.out.println("-----------------------------------");
     }
 }
